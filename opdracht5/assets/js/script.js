5a83fb6ea4a8f6a6a15b6d5e052166d0c4b0e3c7
Isis <isisvanhemert@gmail.com>
2023-02-22 12:10:41 +0100

    sapen !!! :D

---
+++ opdracht5/assets/js/script.js
@@ -0,0 +1,80 @@
+var crashCounter = 0;
+
+for (let boomaantallen = 0; boomaantallen < 5; boomaantallen++) {
+    var img = document.createElement('img');
+    img.src = 'assets/img/tree.jpg';
+    img.className = 'tree';
+    img.style.left = Math.random(80) * 80 + '%';
+    img.style.top = Math.random(80) * 80 + '%';
+    document.getElementById('container').appendChild(img);
+}
+
+var car = document.getElementById();
+car.style.left = 100 + 'px';
+car.style.top = 50 + 'px';
+
+document.body.onkeydown = function (event) {
+    var keycode = event.keyCode;
+
+    switch (keycode) {
+        case 40:
+            car.style.transform = 'rotate(90deg)';
+            car.style.top = parseInt(car.style.top) + 5 + 'px';
+            if (crash() == true)
+            {
+                car.style.top = parseInt(car.style.top) - 5 + 'px';
+            }
+            break;
+
+        case 37:
+            car.style.transform = 'rotate(180deg)';
+            car.style.left = parseInt(car.style.left) - 5 + 'px';
+            if (crash() == true)
+            {
+                car.style.top = parseInt(car.style.top) + 5 + 'px';
+            }
+            break;
+
+        case 39:
+            car.style.transform = 'rotate(0deg)';
+            car.style.left = parseInt(car.style.top) + 5 + 'px';
+            if (crash() == true)
+            {
+                car.style.top = parseInt(car.style.top) - 5 + 'px';
+            }
+            break;
+
+        case 38:
+            car.style.transform = 'rotate(-90deg)';
+            car.style.top = parseInt(car.style.top) - 5 + 'px';
+            if (crash() == true)
+            {
+                car.style.top = parseInt(car.style.top) + 5 + 'px';
+            }
+            break;
+
+        default: console.log('een andere toets is ingedrukt:    ' + keycode);
+    }
+}
+
+function crash() {
+    var trees = document.getElementsByClassName('tree');
+    var overlap = false;
+
+    for (let boomaantallen = 0; boomaantallen < trees.length; boomaantallen++) {
+        overlap = !(
+            car.getBoundingClientRect().right       < trees[boomaantallen].getBoundingClientRect().left ||
+            car.getBoundingClientRect().left            > trees[boomaantallen].getBoundingClientRect().right ||
+            car.getBoundingClientRect().bottom < trees[boomaantallen].getBoundingClientRect().top ||
+            car.getBoundingClientRect().top             > trees[boomaantallen].getBoundingClientRect().bottom
+        );
+
+        if (overlap) {
+            crashCounter += 1;
+            console.log("boem");
+            return  true;
+        }
+    }
+
+    return false;
+}
